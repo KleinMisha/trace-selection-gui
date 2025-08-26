1e249df41aa8002468be141cce0bc21ac0d8e4e0
MishaKlein <klein.misha@gmail.com>
2025-08-26 15:20:57 +0200

    (ItemList) Factory: done. satisfies unit tests.

---
+++ app/item_list/item_list_factory.py
@@ -0,0 +1,23 @@
+"""
+Factory: Takes care of instantiating this component's MVC, and creates a Controller that the MainController can communicate with.
+
+NOTE: Because this will be a popup window ---> We do not pass it a placeholder widget, but let MainController know of this factory
+"""
+
+from typing import Optional
+
+from PyQt6.QtWidgets import QWidget
+
+from app.item_list.item_list_controller import ItemListController
+from app.item_list.item_list_model import ItemList as ItemListModel
+from app.item_list.item_list_view import PyQtView as ItemListView
+
+
+def create_item_list(
+    items: list[str], title: Optional[str] = None
+) -> ItemListController:
+    """To be called by the main.py when setting up the entire app"""
+    model = ItemListModel(items)
+    view = ItemListView(title=title)
+    controller = ItemListController(model, view)
+    return controller
