22b710424f5c14d4a880949697386fc9f9b2a943
MishaKlein <klein.misha@gmail.com>
2025-08-26 13:00:19 +0200

    added parent widget into initializer for it to be constructed using placeholder in main view.

--- app/interactive_plot/plot_view.py
+++ app/interactive_plot/plot_view.py
@@ -10,7 +10,7 @@ from matplotlib.backend_bases import Event, MouseButton, MouseEvent
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from numpy.typing import NDArray
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtWidgets import QVBoxLayout, QWidget
 
 from app.interactive_plot.plot_view_ui import Ui_InteractivePlot
@@ -36,8 +36,8 @@ class InterActivePlotView(QWidget, Ui_InteractivePlot):
     _adjusted_t_min_signal = pyqtSignal(str)
     _adjusted_t_max_signal = pyqtSignal(str)
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, parent: QWidget | None = None) -> None:
+        super().__init__(parent)
         self.build_ui()
 
         # connect emitting signals
