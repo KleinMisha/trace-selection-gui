01d12d9bd8758f164909a72f0e3f3bfe813796e4
MishaKlein <klein.misha@gmail.com>
2025-08-29 17:04:46 +0200

    Main entry-point to run the application

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+"""
+Main entry-point to start running the full application.
+
+Stich everything together.
+Instantiate all the specific components, and start the Model,View and Controller
+"""
+
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from app.interactive_plot.plot_factory import create_plot_controller
+from app.item_list.item_list_factory import create_item_list
+from app.label_assignment.label_panel_factory import create_label_panel
+from app.main_app.main_controller import ComponentControllers, MainController
+from app.main_app.main_model import MainModel
+from app.main_app.main_view import MainView
+from app.section_label_assignment.sections_panel_factory import create_sections_panel
+
+
 def main():
-    print("Hello from trace-selection-gui!")
+    # must start with a QApplication before creating QWidgets
+    app = QApplication(sys.argv)
+
+    # Start setting up the main controller
+    model = MainModel()
+    view = MainView()
+    components: ComponentControllers = {
+        "interactive_plot": create_plot_controller(
+            placeholder=view.InterActivePlotView
+        ),
+        "label_panel": create_label_panel(placeholder=view.LabelPanelView),
+        "sections_panel": create_sections_panel(placeholder=view.SectionsPanelView),
+        "item_list": create_item_list,
+    }
+    controller = MainController(model, view, components=components)
+
+    # start the application
+    _ = controller
+    view.show()
+    app.exec()
 
 
 if __name__ == "__main__":
