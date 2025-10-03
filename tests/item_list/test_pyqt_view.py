4be7a3f0926daa87a54da0702bab939af00b4c9c
KleinMisha <klein.misha@gmail.com>
2025-10-03 17:43:08 +0200

    allow for entering a new label by using the ENTER key (no need to use mouse to click on the add button every time).

--- tests/item_list/test_pyqt_view.py
+++ tests/item_list/test_pyqt_view.py
@@ -3,6 +3,7 @@ Test listening to input and emitting signals accordingly.
 NOTE: Simulates user clicks with pyQt specific functionalities. So that part is not framework agnostic.
 """
 
+from PyQt6.QtCore import Qt
 from pytestqt.qtbot import QtBot
 
 from app.item_list.item_list_view import PyQtView
@@ -58,3 +59,19 @@ def test_closing_window() -> None:
     view.connect_close_window(mock_signal_handler)
     view.close()
     assert received_signals == ["close"]
+
+
+def test_add_label_using_enter_key(qtbot: QtBot) -> None:
+    """test that adding a new label after pressing the return key does the same as clicking the add button"""
+    view = PyQtView()
+    received_signal = []
+
+    def mock_signal_handler(signal: str) -> None:
+        """mimic the controller-side function that has to get the signal emitted by the view"""
+        received_signal.append(signal)
+
+    view.connect_add_item(mock_signal_handler)
+    qtbot.addWidget(view)
+    view.input_label.setText("entered text")
+    qtbot.keyPress(view.input_label, Qt.Key.Key_Return)
+    assert received_signal[0] == "entered text"
